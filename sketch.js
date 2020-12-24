52d222a902d7233d8d49fe3fe714c271246e312f
sherwindavis <51351591+sherwindavis@users.noreply.github.com>
2020-12-24 18:23:09 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -3,7 +3,7 @@ var no = document.getElementById("barnos").value
 var canvas = document.querySelector("canvas");
 var c = canvas.getContext("2d");
 var hsize = 400;
-var wsize = 800;
+var wsize = 650;
 canvas.style.width = wsize + "px";
 canvas.style.height = hsize + "px";
 var scale = 2; 
@@ -15,14 +15,18 @@ c.scale(scale, scale)
 for(i=0;i<no;i++)
 {
 gap=gap+10
-c.fillStyle= 'white';
-c.strokeStyle='black';
+c.fillStyle= '#80deea';
+c.strokeStyle='#ffffff';
 var randheight = random()
 c.fillRect(gap,0,10,randheight);
 c.strokeRect(gap,0,10,randheight);
 
 }
 function random(){
-    return Math.floor((Math.random() * 250) + 1);
+    return Math.floor((Math.random() * 300) + 1);
 }
 }
+
+
+
+
