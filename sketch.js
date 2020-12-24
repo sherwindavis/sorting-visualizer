9a6c08c6fd575f195f43b67d785b6f3d4926e229
sherwindavis <51351591+sherwindavis@users.noreply.github.com>
2020-12-24 14:08:47 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -2,18 +2,27 @@ function init(){
 var no = document.getElementById("barnos").value
 var canvas = document.querySelector("canvas");
 var c = canvas.getContext("2d");
-var gap=10;
+var hsize = 400;
+var wsize = 1000;
+canvas.style.width = wsize + "px";
+canvas.style.height = hsize + "px";
+var scale = 2; 
+canvas.width = Math.floor(wsize * scale);
+canvas.height = Math.floor(hsize * scale);
+var gap=0;
 c.clearRect(0, 0, canvas.width, canvas.height);
+c.scale(scale, scale)
 for(i=0;i<no;i++)
 {
-gap=gap+3
+gap=gap+10
 c.fillStyle= 'white';
 c.strokeStyle='black';
 var randheight = random()
-c.strokeRect(gap,20,2,randheight);
-c.fillRect(gap,20,2,randheight);
+c.fillRect(gap,0,10,randheight);
+c.strokeRect(gap,0,10,randheight);
+
 }
 function random(){
-    return Math.floor((Math.random() * 100) + 1);
+    return Math.floor((Math.random() * 250) + 1);
 }
 }
